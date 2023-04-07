6db5a708f0fc789a6aabaca76923a766a1377af6
julx134 <julesgammad56@gmail.com>
2023-04-07 19:20:14 -0400

    Fixed update map bug to update rows and col of map file correctly

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from typing import Union
 from pydantic import BaseModel
 from fastapi import FastAPI, HTTPException
 from fastapi.encoders import jsonable_encoder
+import os
 
 app = FastAPI()
 
@@ -30,19 +31,40 @@ def get_map():
 
 @app.put("/map", response_model=MapItem)
 def update_map(item: MapItem):
+    # unpack data
     data = jsonable_encoder(item)
+
+    # throw error for invalid dimensions
     if int(data['row']) <= 0 or int(data['col']) <= 0:
         raise HTTPException(status_code=400, detail="Invalid request. Dimensions must be above 0")
 
-    row, col, map_file = get_map_file()
+    # get old map dimensions and content
+    old_row, old_col, map_file = get_map_file()
     row = int(data['row'])
     col = int(data['col'])
 
-    map_file = map_file[:row]
-    for i, row_element in enumerate(map_file):
-        map_file[i] = row_element[:col]
+    # check cases for adding/removig rows and columns
+    if row > old_row:
+        for i in range(row-old_row):
+            map_file.append(['0'] * old_col)
+    else:
+        map_file = map_file[:row]
 
-    print(map_file)
+    if col < old_col:
+        for i, row_element in enumerate(map_file):
+            map_file[i] = row_element[:col]
+    else:
+        for row_element in map_file:
+            for i in range(old_col, col):
+                row_element.append('0')
+
+    # write to map
+    absolute_path = os.path.dirname(__file__)
+    with open(os.path.join(absolute_path, 'map.txt'), 'w+') as txt_file:
+        txt_file.write(" ".join([data['row'], data['col']]) + '\n')
+        for line in map_file:
+            txt_file.write(" ".join(line) + '\n')
+
     return {"row": data['row'], "col": data['col'], "map": map_file}
 
 
